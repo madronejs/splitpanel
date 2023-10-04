7f67a88697783527a720eb492d9f9b2dfac4d31e
Tyler Reardon <tdr.reardon@gmail.com>
2023-10-04 13:27:12 -0700

    cleanup animate

--- src/plugins/animate.ts
+++ src/plugins/animate.ts
@@ -1,48 +1,33 @@
 import anime from 'animejs';
 import {
-  relativeToPercent, exactToPx, type getSizeInfo, AnimateStrategy, negateChildren, ConstraintType,
+  relativeToPercent, exactToPx, type getSizeInfo, AnimateStrategy, negateChildren,
 } from '@/core/defs';
 import type SplitPanel from '@/core/SplitPanel';
 
 export default function configureAnimate(config?: Omit<anime.AnimeAnimParams, 'update'>): AnimateStrategy {
   return (panel: SplitPanel, items: SplitPanel[], sizeInfo?: ReturnType<typeof getSizeInfo>) => {
-    const panelMap: Record<string, {
-      item: SplitPanel,
-      size: ConstraintType,
-    }> = {};
     const others = negateChildren(panel, items);
-    const vals = items.map((item) => {
-      const newSize = sizeInfo?.relative == null || sizeInfo.relative
-        ? relativeToPercent(item.sizeInfo.relativeSize)
-        : exactToPx(item.sizeInfo.exactSize);
-
-      panelMap[item.id] = {
-        item,
-        size: newSize,
-      };
-      return panelMap[item.id];
-    });
-
+    const onUpdate = () => panel.satisfyConstraints({ incremental: true, items: others });
     const timeline = anime.timeline({
-      update: () => {
-        panel.satisfyConstraints({ incremental: true, items: others });
-      },
+      update: onUpdate,
+      complete: onUpdate,
     });
 
     for (const item of items) {
-      const itemVal = panelMap[item.id];
+      const startSize = sizeInfo?.relative == null || sizeInfo.relative
+        ? relativeToPercent(item.sizeInfo.relativeSize)
+        : exactToPx(item.sizeInfo.exactSize);
+      const targets = { size: startSize };
 
       timeline
         .add({
-          duration: config?.duration ?? 750,
+          duration: config?.duration ?? 1000,
           easing: 'easeInOutQuart',
           size: sizeInfo?.formatted ?? item.originalSize,
           ...config,
-          targets: itemVal,
+          targets,
           update: () => {
-            for (const id of Object.keys(panelMap)) {
-              panelMap[id].item.setSize(panelMap[id].size);
-            }
+            item.setSize(targets.size);
           },
         }, 0);
     }
