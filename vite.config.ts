6b467f8a638265c68ebb59f4cfc50b808d5171ff
Tyler Reardon <tdr.reardon@gmail.com>
2023-10-11 15:32:36 -0700

    remove terser from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 /* eslint-disable unicorn/prefer-module */
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
-import terser from '@rollup/plugin-terser';
 import jsxPlugin from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
   plugins: [jsxPlugin()],
   build: {
-    minify: 'terser',
     cssMinify: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points
@@ -22,12 +20,6 @@ export default defineConfig({
       name: 'SplitPanel',
     },
     rollupOptions: {
-      plugins: [
-        terser({
-          compress: true,
-          mangle: true,
-        }),
-      ],
       // make sure to externalize deps that shouldn't be bundled
       // into your library
       external: ['vue', '@madronejs/core'],
