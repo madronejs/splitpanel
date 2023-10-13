d371841e93d0444399b3c4d93be39ce2ffc15b59
Tyler Reardon <tdr.reardon@gmail.com>
2023-10-12 17:53:12 -0700

    chore: dragging updates (#5)
    - add more ways to customize drag ghost
    - auto set direction of child nodes
    - classes for dragging/dropping

--- src/plugins/draggable.ts
+++ src/plugins/draggable.ts
@@ -1,44 +1,61 @@
 import { watch } from '@madronejs/core';
-import { type DraggableStrategy, type DraggableStrategyReturn } from '@/core/defs';
+import { type DraggableStrategy, type DraggableStrategyReturn, BoxCoord } from '@/core/defs';
 import type SplitPanel from '@/core/SplitPanel';
 
 import 'drag-drop-touch';
 
-function addClone(event: DragEvent, el) {
+function onDragOver(e: DragEvent) {
+  e.preventDefault();
+}
+
+function addClone(event: DragEvent, el: HTMLElement, anchor: BoxCoord) {
   if (!event.dataTransfer) return undefined;
 
   // make a deep clone of the template/invisible ghost,
-  const cloned = el.cloneNode(true);
+  const cloned = el?.cloneNode(true) as HTMLElement;
 
-  // temporarily put it way off screen so we can't see it
-  cloned.style.position = 'fixed';
-  cloned.style.top = '-100vh';
-  cloned.style.left = '-100vw';
-  cloned.style.display = 'block';
+  if (cloned) {
+    // temporarily put it way off screen so we can't see it
+    cloned.style.position = 'fixed';
+    cloned.style.top = '-100vh';
+    cloned.style.left = '-100vw';
+    cloned.style.display = 'block';
+    document.body.append(cloned);
 
-  document.body.append(cloned);
+    const { width, height } = cloned.getBoundingClientRect();
 
-  const { width, height } = cloned.getBoundingClientRect();
+    event.dataTransfer.setDragImage(cloned, width * anchor.x, height * anchor.y);
+  }
 
-  event.dataTransfer.setDragImage(cloned, width * 0.5, height);
   return cloned;
 }
 
-function onDragOver(e: DragEvent) {
-  e.preventDefault();
+function checkDrag(panel: SplitPanel, canDrag?: (panel: SplitPanel) => boolean) {
+  return !panel.numChildren && (canDrag === undefined || canDrag?.(panel) === true);
+}
+
+function checkDrop(panel: SplitPanel, canDrop?: (panel: SplitPanel) => boolean) {
+  return !panel.numChildren && (canDrop === undefined || canDrop?.(panel) === true);
 }
 
 export default function configureDraggable(
   options?: {
     dragSelector?: string,
     dropSelector?: string,
+    draggingClass?: string,
+    dropzoneClass?: string,
     canDrag?: (panel: SplitPanel) => boolean,
     canDrop?: (panel: SplitPanel) => boolean,
+    onDrop?: (opts: { target: SplitPanel, panel: SplitPanel }) => void,
+    ghost?: (panel: SplitPanel) => HTMLElement,
+    ghostAnchor?: (panel: SplitPanel) => BoxCoord,
   }
 ): DraggableStrategy {
   return (panel: SplitPanel) => {
     const dragSelector = options?.dragSelector ?? '.drag-handle';
     const dropSelector = options?.dropSelector;
+    const draggingClass = options?.draggingClass ?? 'split-panel-dragging';
+    const dropzoneClass = options?.dropzoneClass ?? 'split-panel-dropzone';
     let unbindSetup: () => void;
     let unbindDrag: () => void;
     let unbindDrop: () => void;
@@ -49,21 +66,15 @@ export default function configureDraggable(
       unbind: undefined,
     };
 
-    function checkDrop() {
-      return !panel.numChildren && (options?.canDrop === undefined || options?.canDrop?.(panel) === true);
-    }
-
     function onDrop(e: DragEvent) {
       const id = e.dataTransfer.getData('text/plain');
       const droppedPanel = panel.byId(id);
 
-      if (droppedPanel) {
-        panel.swapData(droppedPanel);
-      }
+      options?.onDrop?.({ target: panel, panel: droppedPanel });
     }
 
     function setupDropIfNeeded() {
-      if (dropZone || !checkDrop()) return;
+      if (dropZone || !checkDrop(panel, options?.canDrop)) return;
 
       dropZone = panel.contentEl?.querySelector(dropSelector) ?? panel.contentEl;
 
@@ -78,18 +89,66 @@ export default function configureDraggable(
       };
     }
 
-    function checkDrag() {
-      return !panel.numChildren && (options?.canDrag === undefined || options?.canDrag?.(panel) === true);
+    let dropzoneClassCleanup: HTMLElement[];
+
+    function removeDropzoneClass() {
+      for (const el of dropzoneClassCleanup || []) {
+        el?.classList.remove(dropzoneClass);
+      }
+
+      dropzoneClassCleanup = undefined;
+    }
+
+    function addDropzoneClass() {
+      removeDropzoneClass();
+      dropzoneClassCleanup = [];
+
+      for (const child of panel.root.allChildren) {
+        if (child.id !== panel.id && checkDrop(child, options?.canDrop) && child.containerEl) {
+          child.containerEl.classList.add(dropzoneClass);
+          dropzoneClassCleanup.push(child.containerEl);
+        }
+      }
+    }
+
+    let dragClassCleanup: HTMLElement[];
+
+    function removeDraggingClass() {
+      for (const el of dragClassCleanup || []) {
+        el?.classList.remove(draggingClass);
+      }
+    }
+
+    function addDraggingClass() {
+      removeDraggingClass();
+
+      if (checkDrag(panel, options?.canDrag) && panel.containerEl) {
+        dragClassCleanup = [panel.containerEl];
+        panel.containerEl.classList.add(draggingClass);
+      }
     }
 
     function onDrag(e: DragEvent) {
-      ghostEl = addClone(e, panel.contentEl);
+      const anchor = options?.ghostAnchor?.(panel);
+      const newAnchor = {
+        x: 0.5,
+        y: 1,
+        ...anchor,
+      };
+
+      ghostEl = addClone(
+        e,
+        options?.ghost?.(panel) ?? panel.contentEl,
+        newAnchor
+      );
       e.dataTransfer.dropEffect = 'move';
       e.dataTransfer.setData('text/plain', panel.id);
+      addDraggingClass();
+      addDropzoneClass();
     }
 
     function setupDragIfNeeded() {
-      if (dragHandle || !checkDrag()) return;
+      if (dragHandle || !checkDrag(panel, options?.canDrag)) return;
 
       dragHandle = panel.contentEl?.querySelector(dragSelector);
 
@@ -108,6 +167,8 @@ export default function configureDraggable(
         dragHandle.removeEventListener('dragend', unbindDrag);
         ghostEl?.remove();
         dragHandle = undefined;
+        removeDraggingClass();
+        removeDropzoneClass();
       };
 
       dragHandle.addEventListener('dragend', unbindDrag);
@@ -119,12 +180,14 @@ export default function configureDraggable(
     }
 
     function setupListeners() {
-      panel.contentEl.addEventListener('mousedown', scan);
-      panel.contentEl.addEventListener('touchstart', scan, { passive: true });
+      const { contentEl } = panel;
+
+      contentEl.addEventListener('mousedown', scan);
+      contentEl.addEventListener('touchstart', scan, { passive: true });
 
       unbindSetup = () => {
-        panel.contentEl.removeEventListener('mousedown', scan);
-        panel.contentEl.removeEventListener('touchstart', scan);
+        contentEl.removeEventListener('mousedown', scan);
+        contentEl.removeEventListener('touchstart', scan);
       };
     }
 
