85abdad9706286c4cca75722a1a08a997fa4edb0
Tyler Reardon <tdr.reardon@gmail.com>
2025-05-16 17:01:47 -0700

    add in more slots

--- src/render/SplitPanelItem.vue
+++ src/render/SplitPanelItem.vue
@@ -2,10 +2,11 @@
 import { onBeforeUnmount, inject } from 'vue';
 import { SplitPanel } from '@/core';
 
-import { SplitPanelItemProps } from './interfaces';
+import { SplitPanelItemProps, SplitPanelViewSlots } from './interfaces';
 import SplitPanelBase from './SplitPanelBase.vue';
 
 const props = defineProps<SplitPanelItemProps>();
+const slots = defineSlots<SplitPanelViewSlots>();
 const splitPanelParent = inject<SplitPanel>('splitPanel');
 const [splitPanel] = splitPanelParent.addChild({ ...props, id: props.itemId });
 
@@ -16,6 +17,24 @@ onBeforeUnmount(() => {
 
 <template>
   <SplitPanelBase :split-panel="splitPanel">
+    <template v-if="slots.resize" #resize="scope">
+      <slot
+        name="resize"
+        v-bind="scope"
+      />
+    </template>
+    <template v-if="slots.ghost" #ghost="scope">
+      <slot
+        name="ghost"
+        v-bind="scope"
+      />
+    </template>
+    <template v-if="slots.dropZone" #dropZone="scope">
+      <slot
+        name="dropZone"
+        v-bind="scope"
+      />
+    </template>
     <template #item="scope">
       <slot v-bind="scope" />
     </template>
