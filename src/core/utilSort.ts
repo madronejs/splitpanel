7b6c04cdea9141a5aa03164c3f9291520a61ad7d
Tyler Reardon <tdr.reardon@gmail.com>
2025-05-19 16:37:18 -0700

    remove extra comments

--- src/core/utilSort.ts
+++ src/core/utilSort.ts
@@ -1,16 +1,10 @@
 import type SplitPanel from './SplitPanel';
 
 export function sortPanels<T extends SplitPanel>(items: T[]) {
-  // Define a helper function to get comparable values, defaulting to sensible values.
   const getMinSize = (item: T) => item.parsedConstraints?.minSize?.relativeValue ?? 0;
   const getMaxSize = (item: T) => item.parsedConstraints?.maxSize?.relativeValue ?? Infinity;
   const hasSize = (item: T) => item.parsedConstraints?.size !== undefined;
 
-  // Prioritize items for resizing:
-  // 1. Items with highest minSize.
-  // 2. Items with lowest maxSize.
-  // 3. Items with parsed size constraints.
-  // 4. Everything else.
   return [...items].sort((a, b) => {
     // 1. Highest minSize
     const minComparison = getMinSize(b) - getMinSize(a);
